73dbde8f7f6cadce74577b25ac4aa92889aa8c75
DANILO BRAYANN <116122936+danilobrayann@users.noreply.github.com>
2023-05-12 11:41:35 -0300

    pagina de uma tela de iphone

---
+++ js/function.js
@@ -0,0 +1,11 @@
+
+const img = document.querySelector(".trocar-imagem-Iphone")
+const container = document.querySelector(".container")
+
+function phones( src){
+ img.src = src
+}
+
+function colors(color){
+container.style.background = color 
+}
